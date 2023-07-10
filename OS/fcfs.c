019f21012911546ca6525601013ac7644616b671
rakshak690 <127379524+rakshak690@users.noreply.github.com>
2023-07-10 15:06:32 +0530

    Update fcfs.c

--- OS/fcfs.c
+++ OS/fcfs.c
@@ -1,89 +1,70 @@
-FCFS
 #include<stdio.h>
-int main()
+ int main()
 {
-char pn[10];
-int arr[10],bur[10],star[10],finish[10],tat[10],wt[10],i,n;
-int totwt=0,tottat=0;
-printf("Enter the number of processes:");
-scanf("%d",&n);
-for(i=0;i<n;i++)
-{
-printf("Enter the Process Name, Arrival Time & Burst Time:");
-scanf("%s%d%d",&pn[i],&arr[i],&bur[i]);
-}
-for(i=0;i<n;i++)
-{
-if(i==0)
-{
-star[i]=arr[i];
-wt[i]=star[i]-arr[i];
-finish[i]=star[i]+bur[i];
-tat[i]=finish[i]-arr[i];
-}
-else
-{
-star[i]=finish[i-1];
-wt[i]=star[i]-arr[i];
-finish[i]=star[i]+bur[i];
-tat[i]=finish[i]-arr[i];
-}
-}
-printf("\nPName Arrtime Burtime Start TAT Finish");
-for(i=0;i<n;i++)
-{
-printf("\n%s\t%6d\t\t%6d\t%6d\t%6d\t%6d",pn[i],arr[i],bur[i],star[i],tat[i],finish[i]);
-totwt+=wt[i];
-tottat+=tat[i];
-}
-printf("\nAverage Waiting time:%f", (float)totwt/n);
-printf("\nAverage Turn Around Time:%f", (float)tottat/n);
+    int burst_time[20],p[20],waiting_time[20],tat[20],i,j,n,total=0,pos,temp;
+    float avg_waiting_time,avg_tat;
+    printf("please enter number of process: ");
+    scanf("%d",&n);
+    printf("\n enter the Burst Time:\n");
+    for(i=0;i<n;i++)
+    {
+        printf("p%d:",i+1);
+        scanf("%d",&burst_time[i]);
+        p[i]=i+1;         
+    }
+   // from here, burst times sorted
+    for(i=0;i<n;i++)
+    {
+        pos=i;
+        for(j=i+1;j<n;j++)
+        {
+            if(burst_time[j]<burst_time[pos])
+                pos=j;
+        }
+        temp=burst_time[i];
+        burst_time[i]=burst_time[pos];
+        burst_time[pos]=temp;
+        temp=p[i];
+        p[i]=p[pos];
+        p[pos]=temp;
+    }
+    waiting_time[0]=0;            
+    for(i=1;i<n;i++)
+    {
+        waiting_time[i]=0;
+        for(j=0;j<i;j++)
+            waiting_time[i]+=burst_time[j];
+        total+=waiting_time[i];
+    }
+    avg_waiting_time=(float)total/n;      
+    total=0;
+    printf("\nProcess\t    Burst Time    \tWaiting Time\tTurnaround Time");
+    for(i=0;i<n;i++)
+    {
+        tat[i]=burst_time[i]+waiting_time[i];   
+        total+=tat[i];
+        printf("\np%d\t\t  %d\t\t    %d\t\t\t%d",p[i],burst_time[i],waiting_time[i],tat[i]);
+    }
+    avg_tat=(float)total/n;    
+    printf("\n\n the average Waiting Time=%f",avg_waiting_time);
+    printf("\n  the average Turnaround Time=%f\n",avg_tat);
 }
 
 
-SOURCE CODE:
-/* A program to simulate the SJF CPU scheduling algorithm */
-#include<stdio.h>
-#include<string.h>
-main()
-{
-Int i=0,pno[10],bt[10],n,wt[10],temp=0,j,tt[10];
-float sum,at;
-printf("\n Enter the no of process ");
-scanf(" %d",&n);
-printf("\n Enter the burst time of each process");
-for(i=0;i<n;i++)
-{
-printf("\n p%d",i);
-scanf("%d",&bt[i]);
-}
-for(i=0;i<n-1;i++)
-{
-for(j=i+1;j<n;j++)
-{
-if(bt[i]>bt[j])
-{
-temp=bt[i];
-bt[i]=bt[j];
-bt[j]=temp;
-temp=pno[i];
-pno[i]=pno[j];
-pno[j]=temp;
-}
-}
-}
-wt[0]=0;
-for(i=1;i<n;i++)
-{
-wt[i]=bt[i-1]+wt[i-1];
-sum=sum+wt[i];
-}
-printf("\n process no \t burst time\t waiting time \t turn around time\n");
-for(i=0;i<n;i++)
-{
-tt[i]=bt[i]+wt[i];
-at+=tt[i];
-printf("\n p%d\t\t%d\t\t%d\t\t%d",i,bt[i],wt[i],tt[i]);
-}
-printf("\n\n\t Average waiting time%f\n\t Average turn around time%f", sum/n,at/n);
-}
+
+/*OUTPUT:please enter number of process: 4
+
+ enter the Burst Time:
+p1:5
+p2:4
+p3:2
+p4:1
+
+Process     Burst Time          Waiting Time    Turnaround Time
+p4                1                 0                   1
+p3                2                 1                   3
+p2                4                 3                   7
+p1                5                 7                   12
+
+ the average Waiting Time=2.750000
+  the average Turnaround Time=5.750000*/
